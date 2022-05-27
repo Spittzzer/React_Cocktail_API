cc0a8526df38a9ba0542a17924aaaefdeca405b5
Bill <billaoun@gmail.com>
2022-05-27 08:20:40 +0200

    initial commit

---
+++ src/context.js
@@ -0,0 +1,64 @@
+import React, { useState, useContext, useEffect } from "react";
+import { useCallback } from "react";
+
+const url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=";
+const AppContext = React.createContext();
+
+const AppProvider = ({ children }) => {
+  const [loading, setLoading] = useState(false);
+  const [searchTerm, setSearchTerm] = useState("a");
+  const [cocktails, setCocktails] = useState([]);
+  const fetchDrinks = useCallback(async () => {
+    try {
+      setLoading(true);
+      const response = await fetch(url + searchTerm);
+      const data = await response.json();
+      const { drinks } = data;
+      console.log(data);
+      if (drinks) {
+        const newCocktails = drinks.map((drink) => {
+          const { idDrink, strDrink, strDrinkThumb, strAlcoholic, strGlass } =
+            drink;
+          return {
+            id: idDrink,
+            name: strDrink,
+            image: strDrinkThumb,
+            info: strAlcoholic,
+            glass: strGlass,
+          };
+        });
+        setCocktails(newCocktails);
+      } else {
+        setCocktails([]);
+      }
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+    }
+  }, [searchTerm]);
+  useEffect(() => {
+    fetchDrinks();
+  }, [searchTerm, fetchDrinks]);
+
+  return (
+    <AppContext.Provider
+      value={{
+        loading,
+        setLoading,
+        searchTerm,
+        setSearchTerm,
+        cocktails,
+        setCocktails,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
+// make sure use
+export const useGlobalContext = () => {
+  return useContext(AppContext);
+};
+
+export { AppContext, AppProvider };
---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Assumenda,
+        quam totam iste hic consequatur quae est velit nihil quibusdam adipisci
+        maxime placeat distinctio nulla labore, ad repellendus consequuntur
+        ipsum. Itaque, commodi? Earum suscipit quod aliquam accusamus illum
+        provident doloremque. Nam?
+      </p>
+    </section>
+  );
+};
+
+export default About;
